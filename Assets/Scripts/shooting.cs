b318881bb72fec13f378ecb15f42c344079e76a7
CaiLewendon <cailewendon@hotmail.com>
2025-01-26 08:25:35 -0800

    fixed Shooting

--- Assets/Scripts/shooting.cs
+++ Assets/Scripts/shooting.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class shooting : MonoBehaviour
@@ -8,6 +9,7 @@ public class shooting : MonoBehaviour
     public float velocity = 10f;
     public float fireRate = 1f; // setting it to zero makes it a lazer beam essentially
     private float nextTimeFireRate = 0f;
+    public GameObject Click_Plane;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,15 +20,27 @@ public class shooting : MonoBehaviour
             cam = Camera.main;
         }
     }
+    
+    private void Awake()
+    {
+        Click_Plane.transform.position = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Click_Plane.transform.position = transform.position;
+        Click_Plane.transform.localScale = new Vector3(cam.orthographicSize * 2f, 1f, cam.orthographicSize * 2f);
+        
         // shoot a raycast from the camera through the mouse position.
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out RaycastHit aim))
         {
+            if (aim.transform.CompareTag("Click_Plane"))
+            {
+                Debug.Log("plane");
+            }
             aimingDir = new Vector3(aim.point.x, transform.position.y, aim.point.z) - transform.position;
             Debug.DrawLine(transform.position, new Vector3(aim.point.x, transform.position.y, aim.point.z));
         }
@@ -34,12 +48,12 @@ public class shooting : MonoBehaviour
         //check if the left mouse button is clicked.
         if (Input.GetMouseButton(0) && Time.time >= nextTimeFireRate)
         {
-            /*if (Physics.Raycast(ray, out RaycastHit hit))
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 // If the raycast hits something, spawn the point at the hit location.
                 Instantiate(sphere, hit.point, Quaternion.identity);
                 
-            }*/
+            }
 
             nextTimeFireRate = Time.time + fireRate;
 
